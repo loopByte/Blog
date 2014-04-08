944e767cf71a70d8657d5b3b53d50784597ed07b
Vasile Milea <milea.vasile959@gmail.com>
2014-04-08 22:31:53 +0300

    Added OGP class

--- include/autoload.class.php
+++ include/autoload.class.php
@@ -6,6 +6,8 @@
     require_once ROOT . DS . 'mysql.class.php';
     require_once ROOT . DS . 'blog.class.php';
     require_once ROOT . DS . 'article.class.php';
+    require_once ROOT . DS . 'ogp.class.php';
     
     $blog = new Blog();
     $article = new Article();
+    $ogp = new OGP();
