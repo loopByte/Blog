291878fe8fe1897f93e2c9f81543bddb8f1a1101
Vasile Milea <milea.vasile959@gmail.com>
2014-04-08 17:45:02 +0300

    Autoload classes

---
+++ include/autoload.class.php
@@ -0,0 +1,11 @@
+<?php
+
+    define('ROOT', dirname(__FILE__));
+    define('DS', DIRECTORY_SEPARATOR);
+    
+    require_once ROOT . DS . 'mysql.class.php';
+    require_once ROOT . DS . 'blog.class.php';
+    require_once ROOT . DS . 'article.class.php';
+    
+    $blog = new Blog();
+    $article = new Article();
