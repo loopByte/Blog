2ced5919ad598c5599a2b602ad6067acdd4a82d2
Vasile Milea <milea.vasile959@gmail.com>
2014-04-10 11:43:29 +0300

    Admin main page

---
+++ admin/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Admin</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="style/default/main.css" />
+    </head>
+<body>
+    <div class="login-box">
+        <form action="" method="POST" >
+            <input type="text" name="username" placeholder="Username" required /><br />
+            <input type="password" name="password" placeholder="Password" required /><br />
+            <input type="submit" value="Log In" /><br />
+        </form>
+    </div>
+</body>
+</html>
