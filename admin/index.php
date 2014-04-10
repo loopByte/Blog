9354be33c58e373e1ca59edd1a4192ad98b9dd98
Vasile Milea <milea.vasile959@gmail.com>
2014-04-10 12:31:28 +0300

    Minor changes

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+    require_once 'include/autoload.class.php';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,12 +11,17 @@
         <link rel="stylesheet" type="text/css" href="style/default/main.css" />
     </head>
 <body>
+    <?php if(!isset($_SESSION['logged'])){ ?>
     <div class="login-box">
-        <form action="" method="POST" >
+        <form action="index.php?action=login" method="POST" >
             <input type="text" name="username" placeholder="Username" required /><br />
             <input type="password" name="password" placeholder="Password" required /><br />
             <input type="submit" value="Log In" /><br />
         </form>
     </div>
+    <?php }else{ ?>
+    
+    
+    <?php } ?>
 </body>
 </html>
