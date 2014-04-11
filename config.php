6167b918fb005713a4d83be8d0cf6ca34ce72eeb
Vasile Milea <milea.vasile959@gmail.com>
2014-04-11 19:06:08 +0300

    Added SMTP configuration

--- config.php
+++ config.php
@@ -4,3 +4,9 @@
     
     $config['title'] = 'PHP Blog';
     $config['description'] = 'Simple blog written in PHP';
+    
+    $config['smtp_host'] = 'smtp.mailgun.org';
+    $config['smtp_username'] = 'postmaster@sandbox7524.mailgun.org';
+    $config['smtp_password'] = '95x3q99uwvp4';
+    $config['smtp_secure'] = 'tls';
+    $config['smtp_from'] = 'postmaster@sandbox7524.mailgun.org';
