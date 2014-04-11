29651da4bce0871195cbadbbe2a458d0f06cbcd3
Vasile Milea <milea.vasile959@gmail.com>
2014-04-11 19:06:31 +0300

    Added some new classes

--- include/autoload.class.php
+++ include/autoload.class.php
@@ -1,13 +1,14 @@
 <?php
-
-    define('ROOT', dirname(__FILE__));
-    define('DS', DIRECTORY_SEPARATOR);
     
-    require_once ROOT . DS . 'mysql.class.php';
-    require_once ROOT . DS . 'blog.class.php';
-    require_once ROOT . DS . 'article.class.php';
-    require_once ROOT . DS . 'ogp.class.php';
+    require_once ROOT . DS . 'include/mysql.class.php';
+    require_once ROOT . DS . 'include/blog.class.php';
+    require_once ROOT . DS . 'include/article.class.php';
+    require_once ROOT . DS . 'include/ogp.class.php';
+    require_once ROOT . DS . 'libraries/PHPMailer/PHPMailerAutoload.php';
+    require_once ROOT . DS . 'include/newsletter.class.php';
     
     $blog = new Blog();
     $article = new Article();
     $ogp = new OGP();
+    $mail = new PHPMailer;
+    $newsletter = new Newsletter();
