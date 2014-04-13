462f053cc2daa809ddebee5ab97d5fa4443d99df
Vasile Milea <milea.vasile959@gmail.com>
2014-04-13 13:20:47 +0300

    Added some methods to manage articles
    -Method to list all articles posted
    -Method to remove article
    -Method to fetch data to edit
    -Method to update article

--- include/article.class.php
+++ include/article.class.php
@@ -67,4 +67,72 @@
                 header("Location: index.php");
             }
         }
+        
+        public static function manage_list(){
+            global $mysqli;
+            
+            $query = "SELECT id, title, postdate FROM articles";
+            $result = $mysqli->query($query);
+            
+            echo '<table border="1" class="manage-article">';
+            echo '<thead>';
+            echo '<tr>';
+            echo '<th>ID</th>';
+            echo '<th>Title</th>';
+            echo '<th>Post date</th>';
+            echo '<th>Actions</th>';
+            echo '</tr>';
+            echo '</thead>';
+            echo '<tbody>';
+            while($row = $result->fetch_assoc()){
+                echo '<tr>';
+                echo '<td>' . $row['id'] . '</td>';
+                echo '<td>' . $row['title'] . '</td>';
+                echo '<td>' . $row['postdate'] . '</td>';
+                echo '<td>
+                    <a href="edit-article.php?article=edit&id='.$row['id'].'">Edit</a>
+                    <a href="manage-article.php?article=remove&id='.$row['id'].'">Delete</a>
+                </td>';
+                echo '</tr>';
+            }
+            echo '</tbody>';
+            echo '</table>';
+        }
+        
+        public static function remove(){
+            if(isset($_GET['article']) && $_GET['article'] == 'remove'){
+                global $mysqli;
+                
+                $query = "DELETE FROM articles WHERE id='".$_GET['id']."'";
+                $mysqli->query($query);
+                header("Location: manage-article.php");
+            }
+        }
+        
+        public static function edit_fetch(){
+            if(isset($_GET['article']) && $_GET['article'] == 'edit'){
+                global $mysqli;
+                
+                $query = "SELECT * FROM articles WHERE id='".$_GET['id']."'";
+                $result = $mysqli->query($query);
+                return $result->fetch_assoc();
+            }
+        }
+        
+        public static function edit(){
+            if(isset($_GET['article']) && $_GET['article'] == 'edit_save'){
+                global $mysqli;
+                
+                $id = $_POST['id'];
+                $title = $_POST['title'];
+                $content = $_POST['content'];
+                $tags = $_POST['tags'];
+                $postdate = date("Y-m-d H:i:s");
+                $wrap = $_POST['wrap'];
+                
+                $query = "UPDATE articles SET title='".$title."', content='".$content."', postdate='".$postdate."', tags='".$tags."', wrap='".$wrap."' WHERE id='".$id."'";
+                $mysqli->query($query);
+                header("Location: manage-article.php");
+            }
+        }
     }
