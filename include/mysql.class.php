a230ba8d04aa23910f46250c23862e09e58da826
Vasile Milea <milea.vasile959@gmail.com>
2014-04-13 13:22:29 +0300

    user and password changed

--- include/mysql.class.php
+++ include/mysql.class.php
@@ -1,6 +1,6 @@
 <?php
 
-    $mysqli = new mysqli('127.0.0.1',  'root', '', 'blog');
+    $mysqli = new mysqli('127.0.0.1',  'blog', 'blog', 'blog');
     
     if($mysqli->connect_errno){
         exit('MySQL error.');
