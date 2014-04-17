a51d55e0f96bd66c43a3a73de6a5c3e6764f1633
Vasile Milea <milea.vasile959@gmail.com>
2014-04-17 19:47:28 +0300

    Included config file

--- admin/include/autoload.class.php
+++ admin/include/autoload.class.php
@@ -1,5 +1,6 @@
 <?php
 
+    require_once '../config.php';
     require_once '../include/mysql.class.php';
     require_once '../include/article.class.php';
     require_once 'include/user.class.php';
